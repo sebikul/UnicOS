53e668a29eabde439f4ae630df6ad9361e1d0aac
Sebastian Kulesz <sebikul@gmail.com>
2015-05-26 22:08:34 -0300

    No se require ejector source set-variables.sh. Por defecto utiliza linux

--- set-variables.sh
+++ set-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$1" = "reset" ]]; then
+if [[ "$1" = "linux" ]]; then
 	unset GCC
 	unset GPP
 	unset LD
@@ -9,7 +9,8 @@ if [[ "$1" = "reset" ]]; then
 	export MACOS=0
 
 	echo "Reseteando variables..."
-else
+elif [[ "$1" == "macos" ]]; then
+	#statements
 	export GCC=/Users/sebikul/Cross64/bin/x86_64-elf-gcc
 	export GPP=/Users/sebikul/Cross64/bin/x86_64-elf-g++
 	export LD=/Users/sebikul/Cross64/bin/x86_64-elf-ld
@@ -18,6 +19,10 @@ else
 	export MACOS=1
 
 	echo "GCC Cross-Compiler configurado..."
+
+else
+
+	echo -e "Uso: source set-variables.sh [linux|macos]\n\tPor defecto se utiliza linux."
 
 fi
 
