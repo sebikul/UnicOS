e822613a03deb2b1bbdf6f9d9281201e3bf0a5c9
Sebastian Kulesz <sebikul@gmail.com>
2015-05-27 18:27:34 -0300

    Permite ejecutar qemu en modo monitor

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 if [[ "$1" == "debug" ]]; then
+	
 	qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
+
+elif [[ "$1" == "monitor" ]]; then
+
+	qemu-system-x86_64 -monitor stdio -hda Image/x64BareBonesImage.qcow2 -m 512
+
 else
+
 	qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+
 fi
 
