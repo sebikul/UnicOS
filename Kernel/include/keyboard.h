294fb6f9e172d1004ec00de47f37980c74e02c2e
Sebastian Kulesz <sebikul@gmail.com>
2015-05-28 00:28:25 -0300

    ?

---
+++ Kernel/include/keyboard.h
@@ -0,0 +1,33 @@
+
+#ifndef HKEYBOARD_H
+#define HKEYBOARD_H 
+
+#define NULL 0
+
+#define TRUE 1
+#define FALSE 0
+
+#define KEYBOARD_BUFFER_SIZE 16
+
+typedef char bool;
+
+typedef struct {
+	char scancode;
+	char ascii;
+} scancode;
+
+//vamos a sacrificar mucho espacio para que el codigo sea mas legible. Se podrian usar mascaras
+typedef struct {
+	bool caps;
+} kstatus;
+
+
+#endif
+
+
+
+
+
+
+
+
