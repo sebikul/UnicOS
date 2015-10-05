3e537b9667e48d2c4581f6e65fe934acf9a42086
Sebastian Kulesz <sebikul@gmail.com>
2015-10-05 16:54:37 -0300

    Cross compiler para MacOS

---
+++ Cross64/lib/gcc/x86_64-elf/4.7.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=x86_64-elf --prefix=/Users/zhiayang/cross --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
