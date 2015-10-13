87e4ee35fc0996127647a382424ba656ab6f019b
Sebastian Kulesz <sebikul@gmail.com>
2015-10-13 00:44:05 -0300

    Al ejecutar, imprimir la salida del puerto serial en stdout

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 if [[ "$1" == "debug" ]]; then
 	
-	qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
+	qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -serial stdio
 
 elif [[ "$1" == "monitor" ]]; then
 
-	qemu-system-x86_64 -monitor stdio -hda Image/x64BareBonesImage.qcow2 -m 512
+	qemu-system-x86_64 -monitor stdio -hda Image/x64BareBonesImage.qcow2 -m 512 -serial stdio
 
 else
 
-	qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+	qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -serial stdio
 
 fi
 
