5a0f270a4d87afdca1f102262f755800cfa5aeb0
Sebastian Kulesz <sebikul@gmail.com>
2015-11-12 14:42:33 -0300

    strncmp

--- headers/string.h
+++ headers/string.h
@@ -5,6 +5,7 @@
 
 void * memcpy(void * destination, const void * source, uint64_t length);
 int strcmp(const char* s1, const char* s2);
+int strncmp(const char* s1, const char* s2, int n);
 char* strcpy(char* dest, const char* src);
 int strpos(const char* s, char n);
 void * memset(void * destiation, char c, uint64_t length);
