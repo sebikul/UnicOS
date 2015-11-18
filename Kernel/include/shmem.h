41a5916efef2ecf822e2f9a1912994b7df63fb51
Matias  Fraga <mfraga@lab102.it.itba.edu.ar>
2015-11-18 17:57:26 -0300

    API de sem y shmem

--- Kernel/include/shmem.h
+++ Kernel/include/shmem.h
@@ -1,17 +1,22 @@
-#include <stdint.h>
+#include "types.h"
 
 typedef struct mpoint_t{
 	uint32_t memid;
 	uint32_t semid;
+	uint32_t user;
 	uint64_t size;
+	uint32_t used;
 	void *shmaddr;
-	uint32_t flags;
+	uint32_t locked;
+	bool r_flag;
 	uint32_t atcount;
 }mpoint_t;
 
 
-mpoint_t *shmget(uint32_t semid, uint64_t size, uint32_t flags);
-int shmctl(uint32_t cmd, mpoint_t *mp);
+mpoint_t *shmget(uint64_t size, uint32_t user);
+bool shmctl(uint32_t cmd, uint32_t user, mpoint_t *mp);
 void shmat(mpoint_t *mp);
 int shmdt(mpoint_t *mp);
+uint32_t shm_read(char* data, uint32_t size , uint32_t user, mpoint_t *mp);
+uint32_t shm_write(char* data, uint32_t size , uint32_t user, mpoint_t *mp);
 void freemem(mpoint_t *mp);
--- Kernel/ipc/smem.c
+++ Kernel/ipc/smem.c
@@ -1,24 +1,110 @@
-#include <stdint.h>
+#include "types.h"
 #include "shmem.h"
 #include "mem.h"
 
+#define SHM_RLOCK 			300
+#define SHM_WLOCK			301
+#define SHM_UNLOCK			302
+#define SHM_RMID			303 
+#define SHM_SETU			304
+
+#define LOCKED_WRITE		400
+#define LOCKED_READ			401
+#define UNLOCKED			402
 
 uint32_t idcont=0;
 
-mpoint_t *shmget(uint32_t semid, uint64_t size, uint32_t flags) {
+
+mpoint_t *shmget(uint64_t size, uint32_t user) {
 	mpoint_t *mem = malloc(sizeof(mpoint_t));
 	mem->memid = idcont++;
-	mem->semid = semid;
+	mem->user = user;
 	mem->size = size;
-	mem->flags = flags;
+	mem->used = 0;
+	mem->locked = UNLOCKED;
+	mem->r_flag = FALSE;
 	mem->atcount = 0;
 	mem->shmaddr = malloc(mem->size);
 
 	return mem;
 }
 
-int shmctl(uint32_t cmd, mpoint_t *mp) {
-	
+bool shmctl(uint32_t cmd, uint32_t user, mpoint_t *mp) {
+
+	switch(cmd) {
+		case SHM_RLOCK: 
+
+			if ( mp->locked != UNLOCKED || mp->user != user ) 
+				return FALSE;
+
+			mp->locked = LOCKED_READ;
+			break;
+
+		case SHM_WLOCK:
+
+			if ( mp->locked != UNLOCKED || mp->user != user ) 
+				return FALSE;
+
+			mp->locked = LOCKED_WRITE;
+			break;
+
+		case SHM_UNLOCK:
+
+			if ( mp->user != user ) 
+				return FALSE;
+
+			mp->locked = UNLOCKED;
+			break;
+
+		case SHM_RMID:
+
+			if ( mp->r_flag ) {
+				mp->r_flag = FALSE;
+			} else {
+				mp->r_flag = TRUE;
+			}
+			break;
+
+		case SHM_SETU:
+
+			mp->user = user;
+			break;
+
+		default:
+
+			return FALSE;			
+	}
+
+	return TRUE;
+}
+
+uint32_t shm_read(char* data, uint32_t size, uint32_t user, mpoint_t *mp) {
+
+	if ( mp->locked != LOCKED_WRITE ) 
+		return -1;
+
+	shmctl(SHM_RLOCK, user, mp);
+
+	uint32_t data_size = (size > mp->used)? mp->used : size;
+	memcpy(data, mp->shmaddr, data_size);
+
+	shmctl(SHM_UNLOCK, user, mp);
+	return data_size;
+}
+
+uint32_t shm_write(char* data, uint32_t size , uint32_t user, mpoint_t *mp) {
+
+	if ( mp->user != user || mp->locked != UNLOCKED )
+		return -1;
+
+	shmctl(SHM_WLOCK, user, mp);
+
+	uint32_t data_size = (size > mp->size)? mp->size : size; 
+	memcpy(mp->shmaddr, data, data_size);
+	mp->used = data_size;
+
+	shmctl(SHM_UNLOCK, user, mp);
+	return data_size;
 }
 
 void shmat(mpoint_t *mp) {
@@ -28,6 +114,11 @@ void shmat(mpoint_t *mp) {
 int shmdt(mpoint_t *mp) {
 	if ( mp->atcount > 0 )
 		mp->atcount--;
+
+	if ( mp->atcount == 0 && mp->r_flag ) {
+		freemem(mp);
+		return NULL;
+	}
 
 	return mp->atcount;
 }
